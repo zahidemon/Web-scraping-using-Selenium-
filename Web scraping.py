e50c388a57b89d8bae299ba305a8f37403ee7826
Abu Zahid Bin Aziz <39363385+zahidemon@users.noreply.github.com>
2020-04-27 11:21:21 +0600

    Update Web scraping.py

--- Web scraping.py
+++ Web scraping.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[35]:
+
 
 
 
@@ -22,191 +22,74 @@ driver= webdriver.Chrome('chromedriver')
 
 driver.get('http://ictcf.biocuckoo.cn/view.php?id=Patient%201')
 
-sleep(5)
-cols=[]
+col=[]
 tab_data = driver.find_element_by_class_name('array1')
 list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
              for row in tab_data.find_elements_by_tag_name('tr')]
 
 for l in list:
     if l[0]:
-        cols.append(l[0][:-1])
-
-
-
-tab_data = driver.find_element_by_id("Routine_Blood Test")
-list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-             for row in tab_data.find_elements_by_tag_name('tr')]
-
-data=[]
-d=[]
-for l in list:
-    if l:
-        cols.append(l[0])
-        #x=l[2].split(" ")
-        #d.append(x[0])
-        
-tab_data = driver.find_element_by_id("Inflammation")
-list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-             for row in tab_data.find_elements_by_tag_name('tr')]
-
-for l in list:
-    if l:
-        cols.append(l[0])
-
-tab_data = driver.find_element_by_id("Blood_Coagulation Test")
-list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-             for row in tab_data.find_elements_by_tag_name('tr')]
-
-for l in list:
-    if l:
-        cols.append(l[0])
-
-tab_data = driver.find_element_by_id("Biochemical_Test")
-list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-             for row in tab_data.find_elements_by_tag_name('tr')]
-
-for l in list:
-    if l:
-        cols.append(l[0])
-        
-        
-tab_data = driver.find_element_by_id("Immune_Cell Typing")
-list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-             for row in tab_data.find_elements_by_tag_name('tr')]
-
-for l in list:
-    if l:
-        cols.append(l[0])
-        
-tab_data = driver.find_element_by_id("Cytokine_Profile Test")
-list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-             for row in tab_data.find_elements_by_tag_name('tr')]
-
-for l in list:
-    if l:
-        cols.append(l[0])
-
-
-
-
-#Code for getting data
-
+        col.append(l[0][:-1])
 
 url='http://ictcf.biocuckoo.cn/view.php?id=Patient%20'
 p_id=1
-data=[]
 for i in range(1170):
     driver.get(url+str(p_id))
-    d=[]
-    sleep(3)
+    sleep(1)
+    tab_data = driver.find_element_by_xpath('//div[@id="container"]/div[@id="mainContent"]/div[@id="CrossRef"]/div[@id="ref"]')
+    list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
+             for row in tab_data.find_elements_by_tag_name('tr')]
+    #print(list)
+    for l in list:
+        if l:
+            if l[0] not in col:
+                col.append(l[0])
+    p_id=p_id+1
+len(col)        
+
+#Code for getting data
+data={}
+for c in col:
+    data[c]=[]
     
-    
+url='http://ictcf.biocuckoo.cn/view.php?id=Patient%20'
+p_id=1
+for i in range(1170):
+    driver.get(url+str(p_id))
+    sleep(1)
     tab_data = driver.find_element_by_class_name('array1')
     list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
                  for row in tab_data.find_elements_by_tag_name('tr')]
 
+    
     for l in list:
-        if l[1]:
-            d.append(l[1])
-    
-    if driver.find_elements_by_xpath('//div[@id="container"]/div[@id="mainContent"]/div[@id="CrossRef"]/div[@id="ref"]/table[@id="Routine_Blood Test"]'):
-    
-        tab_data = driver.find_element_by_id("Routine_Blood Test")
-        list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-                     for row in tab_data.find_elements_by_tag_name('tr')]
-
-    
-        for l in list:
-            if l:
-                #cols.append(l[0])
-                x=l[2].split(" ")
-                d.append(x[0])
-    else:
-        for j in range(24):
-            d.append(nan)
-    if driver.find_elements_by_xpath('//div[@id="container"]/div[@id="mainContent"]/div[@id="CrossRef"]/div[@id="ref"]/table[@id="Inflammation"]'):
-    
-        tab_data = driver.find_element_by_id("Inflammation")
-        list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-                     for row in tab_data.find_elements_by_tag_name('tr')]
-
-        for l in list:
-            if l:
-                x=l[2].split(" ")
-                d.append(x[0])
-    else:
-        for j in range(3):
-            d.append(nan)
-    if driver.find_elements_by_xpath('//div[@id="container"]/div[@id="mainContent"]/div[@id="CrossRef"]/div[@id="ref"]/table[@id="Blood_Coagulation Test"]'):
-    
-        tab_data = driver.find_element_by_id("Blood_Coagulation Test")
-        list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-                     for row in tab_data.find_elements_by_tag_name('tr')]
-
-        for l in list:
-            if l:
-                x=l[2].split(" ")
-                d.append(x[0])
-    else:
-        for j in range(6):
-            d.append(nan)
-    if driver.find_elements_by_xpath('//div[@id="container"]/div[@id="mainContent"]/div[@id="CrossRef"]/div[@id="ref"]/table[@id="Biochemical_Test"]'):
-    
-        tab_data = driver.find_element_by_id("Biochemical_Test")
-        list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-                     for row in tab_data.find_elements_by_tag_name('tr')]
-
-        for l in list:
-            if l:
-                x=l[2].split(" ")
-                d.append(x[0])
-    else:
-        for j in range(38):
-            d.append(nan)
-
-    if driver.find_elements_by_xpath('//div[@id="container"]/div[@id="mainContent"]/div[@id="CrossRef"]/div[@id="ref"]/table[@id="Immune_Cell Typing"]'):
-        tab_data = driver.find_element_by_id("Immune_Cell Typing")
-        list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-                     for row in tab_data.find_elements_by_tag_name('tr')]
-
-        for l in list:
-            if l:
-                x=l[2].split(" ")
-                d.append(x[0])
-    else:
-        for j in range(6):
-            d.append(nan)
-    if driver.find_elements_by_xpath('//div[@id="container"]/div[@id="mainContent"]/div[@id="CrossRef"]/div[@id="ref"]/table[@id="Cytokine_Profile Test"]'):
-    
-        tab_data = driver.find_element_by_id("Cytokine_Profile Test")
-        list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
-                     for row in tab_data.find_elements_by_tag_name('tr')]
-
-        for l in list:
-            if l:
-                x=l[2].split(" ")
-                d.append(x[0])
-    else:
-        for j in range(6):
-            d.append(nan)
-            
-            
+        if l[0]:
+            data[l[0][:-1]].append(l[1])
+    tab_data = driver.find_element_by_xpath('//div[@id="container"]/div[@id="mainContent"]/div[@id="CrossRef"]/div[@id="ref"]')
+    list= [[cell.text for cell in row.find_elements_by_tag_name('td')]
+             for row in tab_data.find_elements_by_tag_name('tr')]
+    #print(list)
+    for l in list:
+        if l:
+            x=l[2].split(" ")
+            data[l[0]].append(x[0])
+    for c in col:
+        if len(data[c])< len(data["Age"]):
+            data[c].append(nan)
     p_id=p_id+1
-    data.append(d)
-    
 
 
 #Convert scraped data to a panda dataframe
 
 
-df = pd.DataFrame(data, columns = cols) 
+df = pd.DataFrame(data)
+
 
 
 #Export the datafram to csv
 
 
-df.to_csv (r'export_dataframe.csv', index = False, header=True)
+df.to_csv(r'Dataset.csv', index = False)
 
 
 
